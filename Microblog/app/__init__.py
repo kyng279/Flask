de81149aaba3f4f877f3ab1c4f60d7f79c1d1fb8
Ky <kyng279@gmail.com>
2019-03-27 11:49:55 +0800

    removed app.run

--- Microblog/app/__init__.py
+++ Microblog/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
 app = Flask(__name__)
-#app.config.from_object(Config)
-#app.run(host='0.0.0.0')
+app.config.from_object(Config)
+
 
 from app import routes
