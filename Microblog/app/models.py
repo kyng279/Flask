fdc09a2e357ab1899bfc8dd276ad6e50febfd707
Ky Nguyen <Ky.Nguyen@anittel.com.au>
2019-04-08 11:01:44 +0800

    added columns to users table in db

--- Microblog/app/models.py
+++ Microblog/app/models.py
@@ -2,12 +2,15 @@ from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin, db.Model): #Inherits from two classes
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash=db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self): # defines how the object is printed
         return '<User {}>'.form(self.username)
@@ -17,6 +20,10 @@ class User(UserMixin, db.Model): #Inherits from two classes
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8').hexdigest())
+        return 'https://www.gravatar.com/avatar/{}?d=identicons&s={}'.format(digest,size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
